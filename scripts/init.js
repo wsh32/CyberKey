51eb881b04fe522e9c8108bc20b44de6c88de0f6
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-04-01 19:00:11 -0700

    Fixed the init js derp

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,7 @@
 (function($){
-  $(function(){
+	$(function(){
 
-    $('.button-collapse').sideNav();
+		$('.slider').slider({full_width: true});
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+	});
+})
