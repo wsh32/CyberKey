851205f9ea2238fa215bdb6992fb14f2fc79877b
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-04-02 00:03:09 -0700

    Attempts to fix the collapsibles

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('.slider').slider({full_width: true});
-	$('.materialboxed').materialbox();
-	$('ul.tabs').tabs();
-	$('.collapsible').collapsible();
-	return false;
+	$('.collapsible').collapsible({
+		accordion : true
+	});
 });
