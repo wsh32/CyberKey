a14ad18331ecd6002fa908ad8ec7730a9badc43e
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-04-01 18:33:56 -0700

    Added the favicon and contact page

---
+++ scripts/submit.js
@@ -0,0 +1,20 @@
+$(function() {
+	$( 'form' ).submit
+	(
+		function()
+		{
+			$.ajax({
+				type: 'GET',
+				url: 'https://data.sparkfun.com/input/o85GgGEzV6S9XvWXQoZD',
+				data: $(this).serialize(),
+				processData: false,
+				contentType: false,
+				success: function(data) {
+					console.log(data);
+					toast("Thank you for your submission. ☺");
+				}
+			});
+			return false;
+		}
+	);
+});
