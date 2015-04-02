2c9d4c9301d9cc2679fa38e7db8cfd111fed382d
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-04-01 23:46:16 -0700

    Changed to a collapsible

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$('.slider').slider({full_width: true});
 	$('.materialboxed').materialbox();
+	$('ul.tabs').tabs();
+	$('.collapsible').collapsible();
 	return false;
 });
