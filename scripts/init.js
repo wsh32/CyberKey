5f4339c794e5ef786cf2e3ecd40aaed6d8e7e6d3
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-04-01 22:14:27 -0700

    Updated init.js

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.slider').slider({full_width: true});
+	$('.slider').slider({full_width: true, Interval: 5000});
 	$('.materialboxed').materialbox();
 	return false;
 });
