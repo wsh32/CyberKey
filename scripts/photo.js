691fbff6fde0da963db0d4fa0e480799ea623eb3
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-05-03 17:48:26 -0700

    debug

--- scripts/photo.js
+++ scripts/photo.js
@@ -6,8 +6,7 @@ $(function() {
 			var fd = new FormData();
 			fd.append('firstname', $('[name="firstname"]').prop('value'));
 			fd.append('lastname', $('[name="lastname"]').prop('value'));
-			fd.append('pic', $('[name="pic"]').prop('value'));
-			
+			fd.append('pic', $('[name="pic"]'));
 			$.ajax({
 				type: 'POST',
 				url: 'photochallenge/submit.php',
@@ -17,6 +16,9 @@ $(function() {
 				contentType: false,
 				success: function(data) {
 					Materialize.toast(data, 4000);
+					console.log(data);
+				},
+				error: function(data)	{
 					console.log(data);
 				}
 			});
